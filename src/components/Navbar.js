4fe9172ed9dcab8583561934e9a3f532f0c7acae
amirandersonjones <amirandersonjones@gmail.com>
2022-03-29 15:14:42 -0500

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 //imports at the top
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+
 
 //function definition for my component-return a single JSX element
-const Navbar = () => {
-    return(
-        <div>
-            <h1>This will be my navbar</h1>
-        </div>
+const Navbar = props => {
+    useEffect (() => {console.log('Navbar component redered or re-rendered!')});
+
+    return (
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <ul className="nav navbar-nav">
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/shop">Shop</Link>
+                </li>
+            </ul>
+            <ul className='nav navbar-nav ml-auto'>
+                <li className="nav-item active ml-auto">
+                    <p className="nav-link m-0">{props.students[Math.floor(Math.random() * props.students.length)] /* pick a random element from the students array */}</p>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
